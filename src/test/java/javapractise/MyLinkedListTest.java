06840fcf598f095d540d7421e3240312a7103ccd
souvik1232 <souvik199831@gmail.com>
2021-01-02 10:31:04 +0530

    Added:Funcion to add nodes to the last

--- src/test/java/javapractise/MyLinkedListTest.java
+++ src/test/java/javapractise/MyLinkedListTest.java
@@ -20,4 +20,19 @@ public class MyLinkedListTest {
                          myLinkedList.tail.equals(myFirstNode);
         Assert.assertTrue(result);
     }
+    //UC3
+    @Test
+    public void given3NumbersWhenAppendedToLinkedListShouldBeAddedToLast() {
+        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> mySecondNode = new MyNode<>(30);
+        MyNode<Integer> myThirdNode = new MyNode<>(70);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.append(mySecondNode);
+        myLinkedList.append(myThirdNode);
+        boolean result = myLinkedList.head.equals(myFirstNode) &&
+                myLinkedList.head.getNext().equals(mySecondNode) &&
+                myLinkedList.tail.equals(myThirdNode);
+        Assert.assertTrue(result);
+    }
 }
